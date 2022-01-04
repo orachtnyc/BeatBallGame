e6fa01d8cd8eba9220c3f6de715c8cdf73ca2bb0
orachtnyc <orachtnyc@gmail.com>
2022-01-04 21:50:06 +0800

    Progress 3

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,12 +8,19 @@ public class Ball : MonoBehaviour
     float _speed = 20f;
     Rigidbody _rigidbody;
     Vector3 _velocity;
+    Renderer _renderer;
 
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _renderer = GetComponent<Renderer>();
+        Invoke("Launch", 0.5f);
+    }
+
+    void Launch()
+    {
         _rigidbody.velocity = Vector3.down * _speed;
     }
 
@@ -22,7 +29,14 @@ public class Ball : MonoBehaviour
     {
         _rigidbody.velocity = _rigidbody.velocity.normalized * _speed;
         _velocity = _rigidbody.velocity;
+
+        if (!_renderer.isVisible)
+        {
+            GameManager.Instance.Balls--;
+            Destroy(gameObject);
+        }
     }
+
     private void OnCollisionEnter(Collision collision)
     {
         _rigidbody.velocity = Vector3.Reflect(_velocity, collision.contacts[0].normal);
--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -4,25 +4,40 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour
 {
-    public int hits = 1;
+    public int hits = 2;
     public int points = 10;
+    public Vector3 rotator;
+    public Material hitMaterial;
+
+    Material _orgMaterial;
+    Renderer _renderer;
     void Start()
     {
+        //transform.Rotate(rotator * (transform.position.x + transform.position.y) * 0.1f);
+        _renderer = GetComponent<Renderer>();
+        _orgMaterial = _renderer.sharedMaterial;
 
     }
 
 
     void Update()
     {
+        //transform.Rotate(rotator * Time.deltaTime);
 
     }
     private void OnCollisionEnter(Collision collision)
     {
         hits--;
+        GameManager.Instance.Score += points;
         if (hits <= 0)
         {
             Destroy(gameObject);
         }
+        _renderer.sharedMaterial = hitMaterial;
+        Invoke("RestoreMaterial", 0.5f);
     }
-
+    void RestoreMaterial()
+    {
+        _renderer.sharedMaterial = _orgMaterial;
+    }
 }
